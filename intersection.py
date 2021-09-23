f82c858b06aafc9e358f71c122d1469da34c417d
humberto1ortega <54865685+humberto1ortega@users.noreply.github.com>
2021-09-23 17:14:12 -0400

    Finished Intersections
    Finished the intersections portion

--- intersection.py
+++ intersection.py
@@ -1,17 +1,73 @@
 #Function to find Line Segment Intersections
 #   Find the intersections between a given set of line segments.
+import math
 
+#function find_intersection takes in two line segments containing two 2d points each and returns whether or
+#not the two intersect, do not, or intersect at infinite points
+def intersects(line1, line2):
+    #place line point values into their appropriate x and y 
+    x1, x2, x3, x4 = line1[0][0], line1[1][0], line2[0][0], line2[1][0]
+    y1, y2, y3, y4 = line1[0][1], line1[1][1], line2[0][1], line2[1][1]
+
+    #create vectors for determining points of intersection
+    dx1 = x2 - x1
+    dx2 = x4 - x3
+    dy1 = y2 - y1
+    dy2 = y4 - y3
+    dx3 = x1 - x3
+    dy3 = y1 - y3
+
+    #find determinants
+    det = dx1 * dy2 - dx2 * dy1
+    det1 = dx1 * dy3 - dx3 * dy1
+    det2 = dx2 * dy3 - dx3 * dy2
+
+    #if one line is the same as another but in the opposite direction then we know the number of intersection points is infinite
+    if(x1 == x4 and y1 == y4 and x2 == x3 and y2 == y3):
+        return math.inf
+
+    #will check if the determinent is 0 in which case, the lines are parallel
+    if det == 0.0:  
+        if dx1:
+            if x1 < x3 < x2 or x1 > x3 > x2:
+                return math.inf  
+        else:
+            if y1 < y3 < y2 or y1 > y3 > y2:
+                return math.inf 
+
+        #if the the line intersects at one of the given line segment end points return that point of intersection
+        if line1[0] == line2[0] or line1[1] == line2[0]:
+            return line2[0]
+        elif line1[0] == line2[1] or line1[1] == line2[1]:
+            return line2[1]
+
+        #if nothing else has run then they are parallel but do not intersect so return none since there is no intersection
+        return None  
+
+    #if the lines are found to intersect but not at any end point, perform the calculations to find the point
+    s = det1 / det
+    t = det2 / det
+
+    if 0.0 < s < 1.0 and 0.0 < t < 1.0:
+        return x1 + t * dx1, y1 + t * dy1
+
+
+#function find_intersections runs to find the intersection points and formats them into a string to be saved to a file later
 def find_intersections(data_array):
     #Write result to this string that will print int txt file
-    result = ''
+    result = "Intersections From a Given Set of Line Segments:"
 
-    #dummy code
-    print(data_array)
+    for i in data_array:
+        result = result + "\nLine: " + str(data_array[0])
 
+    result = result + "\n\n"
 
-
-
-
+    for k,i in enumerate(data_array):
+        for j in data_array[k:]:
+            if(i != j):
+                intersection_point = intersects(((i[0][0],i[0][1]), (j[0][0],j[0][1])),((i[1][0],i[1][1]), (j[1][0],j[1][1])))
+                if(intersection_point != None):
+                    result = result + "Line Segment " + "(" + str(i[0][0]) + "," + str(i[0][1]) + ")(" + str(j[0][0]) + "," + str(j[0][1]) + ") INTERSECTS (" + str(i[1][0]) + "," + str(i[1][1]) + ")(" + str(j[1][0]) + "," + str(j[1][1]) + ") AT POINT " + str(intersection_point) + "\n"
 
 
     return result
--- main.py
+++ main.py
@@ -33,14 +33,45 @@ def array_to_points(data_array):
         count = count + 1
     return arr
     
+#function array_to_lines converts a list of points in a 3d array into a a set of line segments
+def array_to_lines(data_array):
+    #Removes the first element of the array since we dont need it anymore (problem type)
+    data_array.pop(0)
+
+    #finds the number of lines in the txt file
+    numb_lines = 0
+    for i in data_array:
+        if(i == "_Line_"):
+            numb_lines += 1
+
+    #create a 3d array each line contains a 2d array with all its points
+    rows, cols, dim = (2, 2, numb_lines)
+    arr = [[[0 for i in range(cols)] for j in range(rows)] for k in range(dim)]
+
+    firstpoint = 1
+    count = 0
+    for i in data_array:
+        if(i != "_Line_"):
+            points = i.split()
+            if(firstpoint == 1):
+                arr[count][0][0] = int(points[0])
+                arr[count][0][1] = int(points[1])
+                firstpoint = 0
+            elif(firstpoint == 0):
+                arr[count][1][0] = int(points[0])
+                arr[count][1][1] = int(points[1])
+                count = count + 1
+                firstpoint = 1   
+
+    return arr
     
 #main class
 class Main:
     @staticmethod
     def __init__(*args: str) -> None:
         #main running code goes here
-        # filename = 'test_txt_files/test_intersection.txt'
-        filename = 'test_txt_files/test_pair_of_points.txt'
+        filename = 'test_txt_files/test_intersection.txt'
+        # filename = 'test_txt_files/test_pair_of_points.txt'
         # filename = 'test_txt_files/test_convex.txt'
         # filename = 'test_txt_files/test_largest_empty_circle.txt'
 
@@ -52,7 +83,7 @@ class Main:
         
         #if the first line of the file asks for intersection run this code
         if(data_array[0] == 'Line Segment Intersection'):
-            results = find_intersections(data_array)
+            results = find_intersections(array_to_lines(data_array))
 
         #else if the first line of the file asks for closest pair of points run this
         elif(data_array[0] == 'Closest Pair Of Points'):
@@ -66,7 +97,7 @@ class Main:
         elif(data_array[0] == 'Largest Empty Circle'):
             results = find_closest_pair(array_to_points(data_array))
 
-        write_txt(results)
+        #write_txt(results)
         print(results)
         return
 
